2559d15a51b6686f8a39c7f1df9ccd368b2f2973
unknown <vrndavana.th@gmail.com>
2020-08-01 15:14:11 -0700

    clean up

--- hines/src/Components/Experience.js
+++ hines/src/Components/Experience.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import {BrowserRouter, Link, Switch, Route } from 'react-router-dom';
 
 function Experience() {
 
--- hines/src/Components/Skills.js
+++ hines/src/Components/Skills.js
@@ -1,7 +1,4 @@
 import React from 'react';
-import {BrowserRouter, Link, Switch, Route } from 'react-router-dom';
-import Home from '../Components/Home';
-
 
 
 

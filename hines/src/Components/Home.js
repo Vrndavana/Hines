6e08648d1508dc776263817c69db72aaf88454a1
unknown <vrndavana.th@gmail.com>
2021-01-25 22:34:51 -0800

    Projects Layout and Info update

--- hines/src/Components/Home.js
+++ hines/src/Components/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Me from '../Images/Me.jpg';
 import {Link} from 'react-router-dom';
 import {Nav} from '../Components/styled';
-// import Projects from '../Components/Projects';
+import Projects from '../Components/Projects';
 
 function Home() {
 
@@ -26,14 +26,14 @@ function Home() {
                 <div class="Base">
                 <div class="BasePlate">
                  <h1 class='Hello'> "Hello World" </h1>
-                 <h2>Davan Hines</h2>
+                 <h2>Mr.Hines</h2>
                  <h3> Welcome to my Portfolio. </h3>
                  <p> Full-Stack Web Dev | Software Engineer | Tech Enthusiast </p>
                 </div>
 
 
                 <div class="Basebutt">
-                 <button class="button">  <a href="#Basement"> <span> Head to the Basement </span> </a>  </button>
+                 <button class="button">  <a href="#Basement"> <span> Projects </span> </a>  </button>
                 </div>
 
                 </div>
@@ -46,10 +46,10 @@ function Home() {
 
                 <div class=" PROJ">
                  <h1 id="Basement" href="Basement"> Projects </h1>  
-                 <h3 id="Basement" href="Basement"> Constructing...</h3> 
-                 {/* <Projects/>  */}
+               
                 </div>
-
+  <Projects/> 
+  
             </section>
 
 
--- hines/src/Components/Projects.js
+++ hines/src/Components/Projects.js
@@ -4,6 +4,9 @@ import React from 'react';
 import Proj1 from '../Images/Proj1.png';
 import Proj2 from '../Images/Proj2.png';
 import Proj3 from '../Images/Proj3.png';
+import JRP from '../Images/JRP.png';
+import Api from '../Images/Api.png';
+import Match from '../Images/Match.png';
 function Projects() {
 
 return(
@@ -27,110 +30,111 @@ return(
       <div class="ProjI">
        <h2 class="ProjIH"> Game of Life </h2>
        <span> JS | HTML | CSS </span>
-       <p> Welcome to my Software Engineer Portfolio.
-         Welcome to my Software Engineer Portfolio.
-         Welcome to my Software Engineer Portfolio. 
-         Welcome to my Software Engineer Portfolio.
-         Welcome to my Software Engineer Portfolio</p>
+       <p> The Game of Life is actually a well known coding game challenge 
+         through the coding community. This "game" is more to test ones
+         skills while also giving a meaningful lesson on community, through
+         the simplified simulation of life and those around us.   </p>
+         <span>(Direct link under construction)</span>
        <section class ="Proj-Buttons"> 
-        <button> Github </button>
+        {/* <button> Github </button>
         <button> Learn More </button>
-        <button> Visit Site </button>  
+        <button> Visit Site </button>   */}
+       </section>
+      </div>
+    </div>
+
+
+
+    <div class='Proj'>
+      <img src={Api} alt="Api" className='Pix'/>
+      <div class="ProjI">
+       <h2 class="ProjIH"> Api Connections </h2>
+       <span> JS | HTML | CSS </span>
+       <p> This project was an API connection objective; To test comprehension 
+         on navagating a Javascript index on the other side of an API using
+         only console logs to locate and identify current positions while pulling data   </p>
+         <span> (Direct link under construction)</span>
+       <section class ="Proj-Buttons"> 
+        {/* <button> Github </button>
+        <button> Learn More </button>
+        <button> Visit Site </button>   */}
        </section>
       </div>
     </div>
 
     <div class='Proj'>
+      <img src={Match} alt="Match Game" className='Pix'/>
+      <div class="ProjI">
+       <h2 class="ProjIH"> Emoji Match </h2>
+       <span> JS | HTML | CSS </span>
+       <p> Match! Match! Match Em Up! Who doesn't love Emojis :3 !!! 
+         This JavaScript rendered project uses state and on click functions
+         to bring you a fun and simple game </p>
+         <span>(Direct link under construction)</span>
+       <section class ="Proj-Buttons"> 
+        {/* <button> Github </button>
+        <button> Learn More </button>
+        <button> Visit Site </button>   */}
+       </section>
+      </div>
+    </div>
+   <div class='Proj'>
       <img src={Proj2} alt="Game of Life" className='Pix'/>
       <div class="ProjI">
        <h2 class="ProjIH"> Bridges To Prosperity </h2>
        <span> JS | HTML | CSS </span>
-       <p> Welcome to my Software Engineer Portfolio.
-         Welcome to my Software Engineer Portfolio.
-         Welcome to my Software Engineer Portfolio. 
-         Welcome to my Software Engineer Portfolio.
-         Welcome to my Software Engineer Portfolio</p>
+       <p> B2P's Map is a project using using JavaScript and Postgres 
+         to connect data from Bridges to Prosperity Api 
+         to be able to track progress of contracts B2P 
+         makes to construct bridges and other structures in 3rd world countries.
+       </p>
+       <span>(Direct link under construction)</span>
        <section class ="Proj-Buttons"> 
-        <button> Github </button>
+        {/* <button> Github </button>
         <button> Learn More </button>
-        <button> Visit Site </button>  
+        <button> Visit Site </button>   */}
        </section>
       </div>
     </div>
-
     <div class='Proj'>
       <img src={Proj3} alt="Game of Life" className='Pix'/>
       <div class="ProjI">
        <h2 class="ProjIH"> Plant Quencher </h2>
        <span> JS | HTML | CSS </span>
-       <p> Welcome to my Software Engineer Portfolio.
-         Welcome to my Software Engineer Portfolio.
-         Welcome to my Software Engineer Portfolio. 
-         Welcome to my Software Engineer Portfolio.
-         Welcome to my Software Engineer Portfolio</p>
+       <p> Keep track of your plants! Simple and easy! Plant Quencher 
+         is a Javascript built app using authentication for login, signup
+         and React components to bring a smooth and easy to follow App that 
+         allows users to see water, soil, and sunlight needs of their plants.
+
+       </p>
+       <span>(Direct link under construction)</span>
        <section class ="Proj-Buttons"> 
-        <button> Github </button>
+        {/* <button> Github </button>
         <button> Learn More </button>
-        <button> Visit Site </button>  
+        <button> Visit Site </button>   */}
        </section>
       </div>
     </div>
 
     <div class='Proj'>
-      <img src={Proj1} alt="Game of Life" className='Pix'/>
+      <img src={JRP} alt="JRP Electric" className='Pix'/>
       <div class="ProjI">
-       <h2 class="ProjIH"> Game of Life </h2>
+       <h2 class="ProjIH"> JRP Electric Inc </h2>
        <span> JS | HTML | CSS </span>
-       <p> Welcome to my Software Engineer Portfolio.
-         Welcome to my Software Engineer Portfolio.
-         Welcome to my Software Engineer Portfolio. 
-         Welcome to my Software Engineer Portfolio.
-         Welcome to my Software Engineer Portfolio</p>
+       <p> JRP Electric builds the structured wiring needed for foundations.
+         This site has recently been contracted to me for updated and fixing, 
+         using a combination of JavaScript and Wordpress coding.
+       </p>
+       <span>(Direct link under construction)</span>
        <section class ="Proj-Buttons"> 
-        <button> Github </button>
+        {/* <button> Github </button>
         <button> Learn More </button>
-        <button> Visit Site </button>  
+        <button> Visit Site </button>   */}
        </section>
       </div>
     </div>
-
-    <div class='Proj'>
-      <img src={Proj2} alt="Game of Life" className='Pix'/>
-      <div class="ProjI">
-       <h2 class="ProjIH"> Game of Life </h2>
-       <span> JS | HTML | CSS </span>
-       <p> Welcome to my Software Engineer Portfolio.
-         Welcome to my Software Engineer Portfolio.
-         Welcome to my Software Engineer Portfolio.         
-         Welcome to my Software Engineer Portfolio.
-         Welcome to my Software Engineer Portfolio </p>
-       <section class ="Proj-Buttons"> 
-        <button> Github </button>
-        <button> Learn More </button>
-        <button> Visit Site </button>  
-       </section>
-      </div>
-    </div>
-
-    <div class='Proj'>
-      <img src={Proj3} alt="Game of Life" className='Pix'/>
-      <div class="ProjI">
-       <h2 class="ProjIH"> Game of Life </h2>
-       <span> JS | HTML | CSS </span>
-       <p> Welcome to my Software Engineer Portfolio.
-         Welcome to my Software Engineer Portfolio.         
-         Welcome to my Software Engineer Portfolio.
-         Welcome to my Software Engineer Portfolio
-         Welcome to my Software Engineer Portfolio. </p>
-       <section class ="Proj-Buttons"> 
-        <button> Github </button>
-        <button> Learn More </button>
-        <button> Visit Site </button>  
-       </section>
-      </div>
-    </div>
-
-
+    
+ 
 
     </div>
   </div>
